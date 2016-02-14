b9f372e543d300916cfcb831d7d8a2bffc8522a4
Christopher Soden <karavol27@gmail.com>
2016-02-14 15:21:59 -0700

    autosave

--- night_writer.rb
+++ night_writer.rb
@@ -16,6 +16,7 @@ class NightWriter
       character_count = plain.length
       finalstring = encode_to_braille(plain)
       @writer.write(finalstring)
+      puts "Created '#{ARGV[1]}' containing #{character_count} characters"
     end
 
     def concatenate_a_line_of_braille(line_number, character_array)
@@ -28,7 +29,7 @@ class NightWriter
 
     def encode_to_braille(input)
       finalstring = ""
-      flag_special_alpha_characters(input)
+      input = flag_special_alpha_characters(input)
       groups = group_characters(input)
       groups.each do |group|
         braille_chars = @translator.alpha_to_braille(group)
@@ -41,10 +42,9 @@ class NightWriter
 
     def group_characters(input)
       groups = []
-      input = flag_special_alpha_characters(input)
       while input.length > 80
         puts input.length
-        if is_special_character(input[79])
+        if is_special_alpha_character(input[79])
           groups << input.slice!(0, 79)
         else
           groups << input.slice!(0, 80)
@@ -56,7 +56,7 @@ class NightWriter
       groups << input
     end
 
-    def is_special_character(character)
+    def is_special_alpha_character(character)
       character == "&"
     end
 
